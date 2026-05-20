f2296a5bced5336dfcec5454ce4471a1dfec66b1
sagorhasan17 <sagorhasan.info80@gmail.com>
2026-05-21 00:12:16 +0600

    added teacher page and showing all the tutor info

--- src/app/(main)/teachers/page.jsx
+++ src/app/(main)/teachers/page.jsx
@@ -1,12 +1,33 @@
+import TutorCard from "@/components/TutorCard";
+import { getAllTutors } from "@/lib/fetchData";
 
-const TeachersPage = () => {
+const TeachersPage = async () => {
+  const allTutorsRes = await getAllTutors();
+
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-center mt-10">Teachers Page</h1>
-      <p className="text-center mt-4 text-lg text-gray-600">
-        This is where you can find all the teachers available on the platform.
-      </p>
-    </div>
+    <section className="min-h-screen bg-slate-50 py-10">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        {/* Tutors */}
+        <div className="mt-6">
+          <div className="mb-10 flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
+            <div>
+              <h2 className="text-3xl font-black text-slate-900">All The Available Tutors</h2>
+
+              <p className="mt-2 text-slate-500">
+                {allTutorsRes.length} tutors available for booking.
+              </p>
+            </div>
+          </div>
+
+          {/* Grid */}
+          <div className="grid gap-6 sm:grid-cols-2 xl:grid-cols-4">
+            {allTutorsRes.map((tutor) => (
+              <TutorCard key={tutor._id} tutor={tutor} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
